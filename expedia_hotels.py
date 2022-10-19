4da83173f98b1ad0319ce267257025da3f67b443
Parth Singh <parthsingh287@gmail.com>
2022-10-19 00:07:56 -0700

    Fetching all hotel details

--- expedia_hotels.py
+++ expedia_hotels.py
@@ -12,6 +12,9 @@ from const import EXPEDIA_RAW_HOTEL_DIR, EXPEDIA_RAW_REVIEW_DIR
 from logger import logger
 from utils import *
 
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="geoapiExercises")
+
 
 class DownloadWorker(Thread):
 
@@ -59,18 +62,29 @@ def save_page(hotel):
 		save_raw_file(driver.page_source, EXPEDIA_RAW_HOTEL_DIR + '1_TRY/', hotel[0]+'.html.gz')
 		# send_raw_file(driver.page_source, EXPEDIA_RAW_DIR + 'RUNDATE_' + str(today) + '/' + loc[0].replace(' (and vicinity)', '') + '/' + str(start_date) + '__' + str(end_date) + '/', 'page.html.gz')
 
+		# Hotel Info
+		hotel_info = {}
+		hotel_info['title'] = driver.find_elements(By.CSS_SELECTOR, '.uitk-spacing-padding-large-blockstart-three .uitk-heading-3')[0].text
+		hotel_info['hotel_id'] = hotel[0]
+		full_address = driver.find_elements(By.CSS_SELECTOR, '.uitk-text-default-theme.uitk-layout-flex-item-flex-basis-full_width')[0].text
+		full_add_arr = full_address.split(',')
+		hotel_info['postal_code'] = full_add_arr[len(full_add_arr)-1].strip()
+		hotel_info['region'] = full_add_arr[len(full_add_arr)-2].strip()
+		hotel_info['locality'] = full_add_arr[len(full_add_arr)-3].strip()
+		hotel_info['street_add'] = ','.join(full_add_arr[0: len(full_add_arr)-3]).strip()
+		
+		hotel_info['latitude'] = driver.find_elements(By.XPATH, "//meta[@itemprop='latitude']")[0].get_attribute('content')
+		hotel_info['longitude'] = driver.find_elements(By.XPATH, "//meta[@itemprop='longitude']")[0].get_attribute('content')		
+		hotel_info['country'] = geolocator.reverse(hotel_info['latitude']+","+hotel_info['longitude']).raw['address'].get('country')
+		print(hotel_info)
 		try:
 			all_reviews_button = driver.find_elements(By.CSS_SELECTOR, '.uitk-button-secondary')
 			for button in all_reviews_button:
-				print("Were" + button.text)
 				if button.text == 'See all reviews' and button.is_enabled():
-					print("here")
 					button.click()
 					save_raw_file(driver.page_source, EXPEDIA_RAW_REVIEW_DIR + '1_TRY/', hotel[0]+'.html.gz')
 					time.sleep(4)
 					break
-				else:
-					return
 
 
 			while driver.find_element(By.CSS_SELECTOR, '.uitk-spacing-margin-block-three .uitk-button-secondary'):
@@ -88,12 +102,31 @@ def save_page(hotel):
 				time.sleep(1)
 				driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 				time.sleep(1)
-			print("saving")
+
+
+			see_more_buttons = driver.find_elements(By.CSS_SELECTOR, '.uitk-link-medium')
+			for button in see_more_buttons:
+				if button.text == 'See more' and button.is_enabled():
+					driver.execute_script("arguments[0].click();", button)
+					time.sleep(0.2)
+					time.sleep(0.2)
+			time.sleep(1)
+
 			save_raw_file(driver.page_source, EXPEDIA_RAW_REVIEW_DIR + '1_TRY/', hotel[0]+'.html.gz')
-
+			
 			reviews = driver.find_elements(By.CSS_SELECTOR, '.uitk-card-content-section-padded')
-
+			ct = 0
+			for review in reviews:
+				if review.text != '':
+					heading = review.find_elements(By.CSS_SELECTOR, '.uitk-heading-5 span')
+					for h in heading:
+						print(heading.text)
+						print("--------\n")
+					print("--------------------------\n")
+					
+			
 		except Exception as e:
+			logger.exception("Exception while fetching reviews")
 			print("error" + str(e))
 			pass
 	except Exception as e:
@@ -137,3 +170,4 @@ def fetch_hotel_pages():
 
 if __name__ == '__main__':
     fetch_hotel_pages()
+    print('\a')
