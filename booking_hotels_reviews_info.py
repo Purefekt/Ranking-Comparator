4b20121ee20eceed3fe10a26910eb04e6b6220b4
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2023-02-27 23:35:27 -0800

    pagination works

--- booking_hotels_reviews_info.py
+++ booking_hotels_reviews_info.py
@@ -1,9 +1,12 @@
 import traceback
 import undetected_chromedriver as uc
-from const import BOOKING_RAW_REVIEW_DIR, CHROME_VERSION
+from const import CHROME_VERSION
 from connector import get_connector
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 
 def save_page(hotel):
@@ -27,7 +30,6 @@ def save_page(hotel):
         # click the reviews button. Note: This works for when there are no reviews as well for some reason
         all_reviews_button = driver.find_elements(By.CSS_SELECTOR, "li.a0661136c9 a[href='#blockdisplay4']")[0]
         all_reviews_button.click()
-        time.sleep(3)
 
         # check if there are no reviews. If none then mark this hotel as 3 in booking_hotels -> flag_reviews
         no_reviews_box = driver.find_elements(By.CSS_SELECTOR, "div.not_enough_reviews")
@@ -44,7 +46,34 @@ def save_page(hotel):
                 driver.close()
                 return
 
+        # Get all the reviews from the first page. Then move to the bottom and move to the 2nd page if it exists
+        ALL_REVIEWS = []
+        driver.set_window_size(width=1200, height=831)
+        time.sleep(0.5)
 
+
+
+
+
+
+        # move to the 2nd page and get all the reviews. Repeat this till next page exists
+        next_page_button = driver.find_elements(By.CSS_SELECTOR, 'a.pagenext')
+        num_pages = 1
+
+
+        while next_page_button:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight * 0.5);")
+            time.sleep(0.5)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(0.5)
+            time.sleep(5)
+
+            next_page_button[0].click()
+            
+            time.sleep(1)
+            next_page_button = driver.find_elements(By.CSS_SELECTOR, 'a.pagenext')
+            num_pages += 1
+        print(f'Number of pages -> {num_pages}')
 
 
 
@@ -73,17 +102,10 @@ def save_page(hotel):
 
 hotels = [
     ('0003faac0e04e1fba74d01e6895e4b18', 'https://www.booking.com/hotel/us/sweet-home-alabama-street-in-houston-central.html?aid=304142&ucfs=1&arphpl=1&checkin=2022-06-18&checkout=2022-06-19&dest_id=20128761&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=1&hapos=426&sr_order=popularity&srpvid=84c9051f875100d9&srepoch=1655513023&all_sr_blocks=636592001_335200376_6_0_0&highlighted_blocks=636592001_335200376_6_0_0&matching_block_id=636592001_335200376_6_0_0&sr_pri_blocks=636592001_335200376_6_0_0__22200&tpi_r=2&from=searchresults#hotelTmpl'),
-    ('0011e3930594d4bf200a5580692f6195', 'https://www.booking.com/hotel/it/residenza-cenisio.html?aid=304142&ucfs=1&arphpl=1&checkin=2022-06-19&checkout=2022-06-20&dest_id=-121726&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=25&hapos=100&sr_order=popularity&srpvid=7762979356610017&srepoch=1655588009&all_sr_blocks=28567101_88453047_2_0_0&highlighted_blocks=28567101_88453047_2_0_0&matching_block_id=28567101_88453047_2_0_0&sr_pri_blocks=28567101_88453047_2_0_0__10200&tpi_r=2&from=searchresults#hotelTmpl'),
+    ('00aa4734d9c6d26830fd291b98087b34', 'https://www.booking.com/hotel/us/courtyard-houston-brookhollow.html?aid=304142&ucfs=1&arphpl=1&checkin=2022-06-17&checkout=2022-06-18&dest_id=20128761&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=17&hapos=292&sr_order=popularity&srpvid=94f602affde5015c&srepoch=1655511776&all_sr_blocks=18102007_91825624_0_34_0&highlighted_blocks=18102007_91825624_0_34_0&matching_block_id=18102007_91825624_0_34_0&sr_pri_blocks=18102007_91825624_0_34_0__8560&tpi_r=2&from=searchresults#hotelTmpl'),
     ('000b844cb054f24182710897d78ac640', 'https://www.booking.com/hotel/us/modern-one-bedroom-located-in-the-texas-medical-center.html?aid=304142&label=gen173nr-1FCAQoggJCDWNpdHlfMjAxMjg3NjFIM1gEaIkCiAEBmAExuAEHyAEM2AEB6AEB-AEDiAIBqAIDuALxoPaeBsACAdICJDA2NTUyYTg5LTg1MjUtNDM4Yy1hNTIxLWRjZjhjOGIwNjFjONgCBeACAQ&ucfs=1&arphpl=1&checkin=2023-02-05&checkout=2023-02-06&dest_id=20128761&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=9&hapos=234&sr_order=popularity&srpvid=59a9a0f8bff201a2&srepoch=1675464818&all_sr_blocks=956790801_368318576_4_0_0&highlighted_blocks=956790801_368318576_4_0_0&matching_block_id=956790801_368318576_4_0_0&sr_pri_blocks=956790801_368318576_4_0_0__18000&from=searchresults#hotelTmpl')
 ]
 
-# hotels = [
-#     ('0003faac0e04e1fba74d01e6895e4b18', 'https://www.booking.com/hotel/us/sweet-home-alabama-street-in-houston-central.html?aid=304142&ucfs=1&arphpl=1&checkin=2022-06-18&checkout=2022-06-19&dest_id=20128761&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=1&hapos=426&sr_order=popularity&srpvid=84c9051f875100d9&srepoch=1655513023&all_sr_blocks=636592001_335200376_6_0_0&highlighted_blocks=636592001_335200376_6_0_0&matching_block_id=636592001_335200376_6_0_0&sr_pri_blocks=636592001_335200376_6_0_0__22200&tpi_r=2&from=searchresults#hotelTmpl')
-# ]
-
-# hotels = [
-#     ('0011e3930594d4bf200a5580692f6195', 'https://www.booking.com/hotel/it/residenza-cenisio.html?aid=304142&ucfs=1&arphpl=1&checkin=2022-06-19&checkout=2022-06-20&dest_id=-121726&dest_type=city&group_adults=2&req_adults=2&no_rooms=1&group_children=0&req_children=0&hpos=25&hapos=100&sr_order=popularity&srpvid=7762979356610017&srepoch=1655588009&all_sr_blocks=28567101_88453047_2_0_0&highlighted_blocks=28567101_88453047_2_0_0&matching_block_id=28567101_88453047_2_0_0&sr_pri_blocks=28567101_88453047_2_0_0__10200&tpi_r=2&from=searchresults#hotelTmpl')
-# ]
 
 for hotel in hotels:
     save_page(hotel)
